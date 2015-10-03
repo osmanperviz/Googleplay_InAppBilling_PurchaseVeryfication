f14aa587df7c8493635b2a6f074273166d406081
osman <osmanperviz@gmail.com>
2015-10-03 22:09:48 +0200

    create google_api_client in play class

--- lib/Googleplay_InAppBilling_PurchaseVeryfication/play.rb
+++ lib/Googleplay_InAppBilling_PurchaseVeryfication/play.rb
@@ -3,6 +3,26 @@ require 'openssl'
 module GoogleplayInAppBillingPurchaseVeryfication
   class Play
 
+    def self.google_api_client
+      config = Config.new
+      @@google_client ||= Google::APIClient.new(
+          auto_refresh_token:  true,
+          application_name:    config.app_name,
+          application_version: config.app_version
+      ).tap do |client|
+
+        client.authorization = Signet::OAuth2::Client.new(
+            :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+            :refresh_token => config.refresh_token,
+            :client_id => config.client_id,
+            :client_secret => config.google_secrets
+        )
+        client.authorization.fetch_access_token!
+      end
+
+    end
+
+
   end
 end
 
