4384fee82b63f600167156b41d0fe1d1d50b5da9
osman <osmanperviz@gmail.com>
2015-10-03 22:58:17 +0200

    add condition load railty class if defined rails

--- lib/Googleplay_InAppBilling_PurchaseVeryfication.rb
+++ lib/Googleplay_InAppBilling_PurchaseVeryfication.rb
@@ -1,7 +1,7 @@
 require "Googleplay_InAppBilling_PurchaseVeryfication/version"
-require 'Googleplay_InAppBilling_PurchaseVeryfication/config'
 require 'Googleplay_InAppBilling_PurchaseVeryfication/play'
-require 'Googleplay_InAppBilling_PurchaseVeryfication/railtie'
+require 'Googleplay_InAppBilling_PurchaseVeryfication/config'
+require 'Googleplay_InAppBilling_PurchaseVeryfication/railtie' if defined?(Rails)
 
 module GoogleplayInAppBillingPurchaseVeryfication
 
