395b09dd1fac0565b5365b4f9006e5d1da4cfdbd
osman <osmanperviz@gmail.com>
2015-11-19 10:05:22 +0100

    change config.rb,use some meta

--- lib/Googleplay_InAppBilling_PurchaseVeryfication/config.rb
+++ lib/Googleplay_InAppBilling_PurchaseVeryfication/config.rb
@@ -10,16 +10,21 @@ module GoogleplayInAppBillingPurchaseVeryfication
       if !File.file?(play_path) && !File.file?(secrets_path)
         raise FileError,'Missing Config File(secrets.yml or google_play.yml)'
       end
-      custum_config = File.file?(play_path) ?  YAML.load_file('config/google_play.yml') :  Hash.new{ |hash, key| hash[key] = {} }
-      secrets_config = File.file?(secrets_path) ?  YAML.load_file('config/secrets.yml') :  Hash.new{ |hash, key| hash[key] = {} }
 
-      @app_name = secrets_config[Rails.env]['app_name'] || custum_config[Rails.env]['app_name']
-      @app_version = secrets_config[Rails.env]['app_version'] || custum_config[Rails.env]['app_version']
-      @refresh_token = secrets_config[Rails.env]['refresh_token'] || custum_config[Rails.env]['refresh_token']
-      @google_secret = secrets_config[Rails.env]['google_secret'] || custum_config[Rails.env]['google_secret']
-      @client_id = secrets_config[Rails.env]['client_id'] || custum_config[Rails.env]['client_id']
+      if File.file?(secrets_path)
+        secrets_config = YAML.load_file('config/secrets.yml')
+        secrets_config[Rails.env].each do |key,value|
+          self.send("#{key}=",value)
+        end
+      else
+        play_config = YAML.load_file('config/google_play.yml')
+        play_config[Rails.env].each do |key,value|
+          self.send("#{key}=",value)
+        end
+      end
+
     end
-
   end
 end
+
 
